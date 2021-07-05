f5c52b3655f48ae4ceb5f5a3faf254a2ba4c0b06
kshvab <kshvab@gmail.com>
2021-07-05 23:59:31 +0200

    fix: fix5

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,37 @@
 const cp = require("child_process");
 const spawn = cp.spawn;
-let child;
+const childProcess = require("child_process");
 
+import AutoGitUpdate from "auto-git-update";
+
+const config = {
+    repository: "https://github.com/kshvab/testApi.git",
+    tempLocation: "C:/logs",
+    ignoreFiles: [],
+    executeOnComplete: " D:\\BTT\\testApp\\start.bat",
+    exitOnComplete: true,
+};
+
+const updater = new AutoGitUpdate(config);
+
+updater.autoUpdate();
+/*
+childProcess.exec(`start start.bat`, function (err, stdout, stderr) {
+    if (err) {
+        console.error(err);
+        return false;
+    }
+});
+setInterval(proceEx, 5000);
+function proceEx() {
+    console.log("You may close this window!");
+
+    process.exit(0);
+}
+*/
+
+/*
+let child;
 const app = () => {
     console.log(888);
     restartApp();
@@ -11,15 +41,26 @@ setInterval(app, 5000);
 export { app };
 
 function restartApp() {
-    spawn("git", ["pull"]); // git pull
-    spawn("yarn", ["install"]);
+    console.log("[xxx] - qBitTask is started");
+
+    //spawn("git", ["pull"]);
+    let child = spawn("start", ["start.bat"]);
+    child.stdout.setEncoding("utf8");
+    child.stdout.on("data", function (data) {
+        var str = data.toString();
+        console.log(str);
+    });
+    //process.exit();
+
+    // git pull
+    //spawn("npm", ["install"]);
     //spawn("yarn", ["install"]).on("close", function () {
-    /*
+    
         if (child) {
             child.kill();
         }
         startApp();
-        */
+        
     // });
 }
 
@@ -36,3 +77,4 @@ function startApp() {
 }
 
 //startApp();
+*/
---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+
+
+
+echo [x] - Hallo from !
+
+TIMEOUT 15
+
+npm i && npm run start
+ 
+
+
+
